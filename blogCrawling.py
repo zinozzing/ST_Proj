c4d8163321b82f024401c9b05dcbaf59004df9e5
zinozzing <rlawlsgh1111@naver.com>
2021-11-18 01:15:58 +0900

    Add make CSV file function

--- blogCrawling.py
+++ blogCrawling.py
@@ -1,5 +1,6 @@
 import requests
 import re
+import csv
 from bs4 import BeautifulSoup
 from urllib.parse import quote
 
@@ -22,7 +23,7 @@ def text_scraping(url):
 
     if soup.find("div", attrs={"class":"se-main-container"}):
         text = soup.find("div", attrs={"class":"se-main-container"}).get_text()
-        text = text.replace("\n","") #공백 제거
+        text = text.replace("\n","") # 공백 제거
         return text
 
     elif soup.find("div", attrs={"id":"postViewArea"}):
@@ -30,8 +31,15 @@ def text_scraping(url):
         text = text.replace("\n","")
         return text
     else:
-        return "네이버 블로그는 맞지만, 확인불가"
+        return "확인 불가"
 
+# CSV 파일 저장 함수
+def toCSV(temp_list):
+    with open('blog_crawling.csv', 'w', encoding='utf-8-sig', newline='') as file : # encoding UTF-8 XXX -> must utf-8-sig
+        csvfile = csv.writer(file)
+        for row in temp_list:
+            csvfile.writerow(row)
+    print("csv 저장 완료")  
 
 def main_crawling(end,option,keyword):
     for i in range(1,end*30,30):
@@ -41,43 +49,60 @@ def main_crawling(end,option,keyword):
 
         headers = {"User-Agent":"mozilla/5.0 (macintosh; intel mac os x 10_15_7) applewebkit/537.36 (khtml, like gecko) chrome/95.0.4638.69 safari/537.36"}
         res = requests.get(url, headers=headers)
-        res.raise_for_status() # 문제시 프로그램 종료
+        res.raise_for_status() # 예외처리
         soup = BeautifulSoup(res.text, "lxml")
 
         posts = soup.find_all("div", attrs={"class":"total_area"})
-
+        temp_list = []
         for post in posts:
-            print("-"*50)
+            # print("-"*50)
+            
             # 제목 크롤링
             post_title = post.find('a',class_='api_txt_lines total_tit').get_text()
-            print("제목 :",post_title)
+            # 확인 원할 시 주석 해제
+            # print("제목 :",post_title) 
             
+            # 확인 원할 시 주석 해제
             # 링크 크롤링
             post_link = post.find("a",class_='api_txt_lines total_tit')['href']
-            print("link :", post_link)
+            # print("link :", post_link)
             
+            # 확인 원할 시 주석 해제
             # 날짜 크롤링
             post_time = post.find("span",class_='sub_time sub_txt').get_text()
-            print("date :", post_time)
-            
-            
+            # print("date :", post_time)
+        
             blog_p = re.compile("blog.naver.com")
             blog_m = blog_p.search(post_link)
-
 
             # 본문 내용 크롤링
             if blog_m:
                 blog_text = text_scraping(delete_iframe(post_link))
-                print("<본문>")
-                print(blog_text)
+                # 확인 원할 시 주석 해제
+                # print("<본문>")
+                # print(blog_text)
                 
             else:
-                print("카페 및 포스트 : 확인 불가")
+                print("Error")
 
-            print("-"*50)
+            # print("-"*50)
+            
+            # CSV를 제작을 위한 리스트
+            temp_list.append([post_title,post_link,post_time,blog_text])
+    return temp_list            
         
 
-num = int(input("1페이지부터 원하는 페이지의 범위: "))
-opt = input("원하는 데이터의 수집 기간(ex. 0 = 전체 , 1 = 1시간, 2 = 1일, 3 = 1주, 4 = 1개월, 5 = 3개월, 6 = 6개월, 7 = 1년): ")
-keyw = input("크롤링을 원하는 단어: ")
-main_crawling(num,opt,keyw)
+
+
+
+### main function section
+def main():
+    num = int(input("1페이지부터 원하는 페이지의 범위: "))
+    opt = input("원하는 데이터의 수집 기간(ex. 0 = 전체 , 1 = 1시간, 2 = 1일, 3 = 1주, 4 = 1개월, 5 = 3개월, 6 = 6개월, 7 = 1년): ")
+    keyw = input("크롤링을 원하는 단어: ")
+    output = main_crawling(num,opt,keyw)
+    # CSV 파일 제작
+    toCSV(output)
+
+if __name__ == "__main__":
+	main()
